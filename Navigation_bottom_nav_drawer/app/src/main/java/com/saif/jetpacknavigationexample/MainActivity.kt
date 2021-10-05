7030e90ff57fc04d0f7deb06b3ce782d4a9f43bf
nabiljaved <nabeeljaved2016@gmail.com>
2021-10-05 14:30:00 +0500

    first commit

---
+++ Navigation_bottom_nav_drawer/app/src/main/java/com/saif/jetpacknavigationexample/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.saif.jetpacknavigationexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar) //this simple line will show toolbar
+
+        val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        //drawer_layout will show hamburger icon
+        appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.homeFragment,R.id.settingsFragment,R.id.notificationsFragment),
+                drawer_layout
+        )
+
+        //it will not show up arrow from bottom navigation
+        setupActionBarWithNavController(navController,appBarConfiguration)
+//        toolbar.setupWithNavController(navController)
+        bottom_nav_view.setupWithNavController(navController)
+        nav_drawer.setupWithNavController(navController)
+
+    }
+
+    //will show up button
+    //Toggle Hamburger icon by passing appBarConfiguration
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when(item.itemId){
+            R.id.item_about_app ->{
+                val action = MainNavGraphDirections.actionGlobalAboutAppFragment()
+                navController.navigate(action)
+                return true
+            }
+            else -> item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+        }
+    }
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.main_menu,menu)
+            return super.onCreateOptionsMenu(menu)
+        }
+}
